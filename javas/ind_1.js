38be7143233d6fba7243fd078e22fbd1fb1242ec
xaviorhsu <adnil.hsu@gmail.com>
2020-04-16 11:31:56 +0800

    4/16

--- javas/ind_1.js
+++ javas/ind_1.js
@@ -1,7 +1,9 @@
 //-----------------------------------------------------------------------------------------------  
-  //websrv.local.tw      //xaviorhsu.github.io/  //var HTTP_IP="tncath.catholic.org.tw/tncath/";
-  var HTTP_IP=(location.hostname=="websrv.local.tw")?'http://websrv.local.tw/hsusF/':'https://tncath.catholic.org.tw/';
-  var nophp  =(location.hostname=="websrv.local.tw")?0:0;
+var $http = (window.location.protocol=="https")? 'https://' : 'http://';
+/* websrv.local.tw/                 xaviorhsu.github.io/ */
+  var HTTP_IP=(location.hostname=="websrv.local.tw")?$http+'websrv.local.tw/hsusF/':$http+'xaviorhsu.github.io/hsusF/';
+  
+  var nophp  =(location.hostname=="websrv.local.tw" || location.hostname=="xaviorhsu.github.io")?0:0;
   var flag   =0; 
 //-----------------------------------------------------------------------------------------------  
   /*var msg ="● 為了資料安全，已改成須以輸入帳密頁面方式才可看到族譜。\n";
