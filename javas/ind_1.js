90a14a3a94b9186e1faf2c84fd20bedf448495b7
xaviorhsu <adnil.hsu@gmail.com>
2020-04-16 11:57:07 +0800

    4/16

--- javas/ind_1.js
+++ javas/ind_1.js
@@ -1,10 +1,7 @@
 //-----------------------------------------------------------------------------------------------  
-var $http = (window.location.protocol=="https")? 'https://' : 'http://';
 /* websrv.local.tw/                 xaviorhsu.github.io/ */
-  var HTTP_IP=(location.hostname=="websrv.local.tw")?$http+'websrv.local.tw/hsusF/':$http+'xaviorhsu.github.io/hsusF/';
-  
-  var flag   =(location.hostname=="websrv.local.tw")?0:0;
-  var nophp  =0; 
+  var HTTP_IP=(location.hostname=="websrv.local.tw")?'http://websrv.local.tw/hsusF/':'https://xaviorhsu.github.io/hsusF/';
+  var flag = 0;  var nophp = 0; 
 //-----------------------------------------------------------------------------------------------  
   /*var msg ="● 為了資料安全，已改成須以輸入帳密頁面方式才可看到族譜。\n";
       msg+="● 請23世、24世輩份的每一房，核對自己的資料，有否錯誤或不足，今後可透過填寫【與我聯絡】按鍵隨時提供正確資訊。";
